a59c84b0330813b22be096d96a05b66d378dbaaa
Riccardo Gualtieri <riccardogualtieribusiness@gmail.com>
2024-06-25 14:55:07 +0200

    x
    x

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -13,12 +13,7 @@ export const BlogPostPreview: FunctionComponent<{
     <div className="break-words">
       <Link href={`/blog/${post.slug}`}>
         <div className="aspect-[16/9] relative">
-          <Image
-            alt={post.title}
-            className="object-cover"
-            src={post.image || "/images/placeholder.webp"}
-            fill
-          />
+          
         </div>
       </Link>
       <div className="grid grid-cols-1 gap-3 md:col-span-2 mt-4">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,7 @@ interface MenuItem {
 const menuItems: MenuItem[] = [
   { name: "Blog", href: "/" },
   { name: "About", href: "/about" },
+  { name: "Aformismi", href: "/aforismi" },
 ];
 export const Navigation: FunctionComponent = () => {
   const pathname = usePathname();
